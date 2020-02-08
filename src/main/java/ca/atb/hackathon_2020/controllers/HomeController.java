a9ca152b8d3582f8fba9c55cac2c722f1c92faf7
Hussain Farbotko-Merabaksh <hfarbotko-merabaksh@payments.ca>
2020-02-08 12:27:28 -0500

    Add home controller

---
+++ src/main/java/ca/atb/hackathon_2020/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package ca.atb.hackathon_2020.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    public String home() {
+        return "home";
+    }
+}
