883af54d62591ba4053ef68e7c14d8d393ae121c
adaccache <adaccache@payments.ca>
2020-02-08 11:26:10 -0700

    working home/spending pages

--- src/main/java/ca/atb/hackathon_2020/controllers/SpendingController.java
+++ src/main/java/ca/atb/hackathon_2020/controllers/SpendingController.java
@@ -1,12 +1,15 @@
 package ca.atb.hackathon_2020.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/yourspending")
 public class SpendingController {
-    public String yourspending() {
+
+    @GetMapping({"/", ""})
+    public String yourSpending() {
         return "yourspending";
     }
 }
